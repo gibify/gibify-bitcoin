1ddc91b4d451ddfd950ff0f530638900aa4daa71
gibify <evandrogibicoski@outlook.com>
2020-10-15 22:48:22 -0300

    ok

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Bitcoin from '../pages/Bitcoin';
+import Profile from '../pages/Profile';
 
 const Routes: React.FC = () => (
     <Switch>
-        <Route path="/" exact component={Bitcoin} />
+        <Route path="/" exact component={Profile} />
+        <Route path="/compre-bitcoin" component={Bitcoin} />
     </Switch>
 );
 
